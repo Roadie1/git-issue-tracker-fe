d2864ced2ece92af219c9fa9a76e82a43aae52b6
Roadie1 <roadie321@mail.ru>
2023-05-23 14:29:56 +0300

    statistics wip

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,5 @@
 import { IssueDTO, IssueUrlParams, Issue } from "./issue.type";
 import { Metadata } from "./metadata.type";
+import { Statistic, StatisticsDTO } from './statistics.type';
 
-export { IssueDTO, Issue, Metadata, IssueUrlParams };
+export { IssueDTO, Issue, Metadata, IssueUrlParams, Statistic, StatisticsDTO };
